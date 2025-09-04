c59c251702955aa7f5887db919c8c84afef7bfd4
Lokendra Nath <nathdeveloper25@gmail.com>
2025-09-04 18:52:55 +0530

    style header

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,26 +1,60 @@
 const Header = () => {
   return (
-    <header className="header">
-      <a href="#" className="logo">
+    <header className="fixed top-3 left-[50%] transform translate-x-[-50%] flex items-center gap-4 bg-[rgba(0,0,0,0.671)] backdrop-blur-md text-white py-2 px-6 rounded-2xl z-50 shadow-2xl">
+      <a
+        href="#"
+        className="text-lg text-nowrap tracking-wide transition duration-300 ease-in-out hover:scale-105"
+      >
         <span>Lokendra Nath</span>
       </a>
-      <ul className="nav-links">
+      <ul className="nav-links flex gap-3">
         <li>
-          <a href="#about">About</a>
+          <a href="#about" className="navbar-a">
+            About
+          </a>
         </li>
         <li>
-          <a href="#skill">Skills</a>
+          <a href="#skill" className="navbar-a">
+            Skills
+          </a>
         </li>
         <li>
-          <a href="#project">Project</a>
+          <a href="#project" className="navbar-a">
+            Project
+          </a>
         </li>
         <li>
-          <a href="#contact">Contact</a>
+          <a href="#contact" className="navbar-a">
+            Contact
+          </a>
         </li>
       </ul>
-      <i className="fa-solid fa-bars" id="menu-icon"></i>
-      <button className="visit-btn">
-        <a href="https://github.com/LokendraNath" target="_blank">
+
+      <div className="hidden">
+        <i className="text-2xl fa-solid fa-bars" id="menu-icon"></i>
+      </div>
+
+      <button
+        className="px-4 py-2 rounded-lg text-white font-medium 
+         bg-gradient-to-r from-[#0077ff] to-[#f001f0] 
+         hover:opacity-90 transition flex"
+      >
+        <svg
+          class="inline-block w-5 h-5 mr-2"
+          fill="currentColor"
+          viewBox="0 0 24 24"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M12 .5C5.37.5 0 5.87 0 12.5c0 5.3 3.44 9.8 8.2 11.4.6.1.82-.26.82-.58v-2.16c-3.34.73-4.04-1.61-4.04-1.61-.54-1.37-1.32-1.74-1.32-1.74-1.08-.74.08-.73.08-.73 1.2.08 1.84 1.23 1.84 1.23 1.06 1.83 2.8 1.3 3.48.99.1-.77.42-1.3.76-1.6-2.66-.3-5.46-1.34-5.46-5.97 0-1.32.47-2.4 1.23-3.24-.12-.3-.54-1.52.12-3.17 0 0 1.01-.32 3.3 1.23a11.5 11.5 0 0 1 6 0c2.3-1.55 3.3-1.23 3.3-1.23.66 1.65.24 2.87.12 3.17.76.84 1.23 1.92 1.23 3.24 0 4.64-2.8 5.67-5.48 5.97.42.36.8 1.06.8 2.14v3.17c0 .32.22.68.82.58A12 12 0 0 0 24 12.5C24 5.87 18.63.5 12 .5Z"
+            clip-rule="evenodd"
+          />
+        </svg>
+        <a
+          href="https://github.com/LokendraNath"
+          target="_blank"
+          className="font-gothic"
+        >
           GitHub
         </a>
       </button>
