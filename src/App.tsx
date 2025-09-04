26efb83ebcf8b2de63862702df4c94ae6ca25132
Lokendra Nath <nathdeveloper25@gmail.com>
2025-09-04 18:00:41 +0530

    fix the error in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import About from "./component/About";
+import Contact from "./component/Contact";
+import Footer from "./component/Footer";
 import Header from "./component/Header";
+import Project from "./component/Project";
+import Skills from "./component/Skills";
 
 const App = () => {
   return (
     <div>
       <Header />
       <About />
+      <Skills />
+      <Project />
+      <Contact />
+      <Footer />
     </div>
   );
 };
