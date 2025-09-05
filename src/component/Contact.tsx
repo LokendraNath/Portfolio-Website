f10f57eb1ee49ccf5b437848b090a7989c397cdb
Lokendra Nath <nathdeveloper25@gmail.com>
2025-09-05 13:06:10 +0530

    contact us

--- src/component/Contact.tsx
+++ src/component/Contact.tsx
@@ -1,17 +1,20 @@
+import rightLogo from "../assets/img/right-logo.png";
+
 const Contact = () => {
   return (
     <section
-      className="contact min-h-screen py-32 px-[12%] w-full relative"
+      className="contact min-h-screen py-20 px-[12%] w-full relative"
       id="contact"
     >
       <div className="contact-left-title">
         <h2 className="section-title">Contact Us</h2>
       </div>
-      <div className="contact-container">
+
+      <div className="contact-container flex items-center justify-center">
         <form
           action="https://api.web3forms.com/submit"
           method="POST"
-          className="contact-left"
+          className="contact-left flex flex-col items-start gap-5"
         >
           <input
             type="hidden"
@@ -22,27 +25,30 @@ const Contact = () => {
             type="text"
             name="name"
             placeholder="Your Name"
-            className="contact-input"
+            className="contactInput"
             required
           />
           <input
             type="email"
             name="email"
             placeholder="Your email"
-            className="contact-input"
+            className="contactInput"
             required
           />
           <textarea
             name="message"
             placeholder="Your Message"
-            className="contact-input"
+            className="h-36 w-full pt-3.5 pl-6 font-medium text-lg rounded-xl bg-gradient-to-r from-[rgba(0,157,255,0.2)] to-[rgba(255,0,255,0.2)] outline-none focus:border focus:border-[#8383ff]"
           ></textarea>
-          <button type="submit">
-            Submit <i className="fa-solid fa-arrow-right"></i>
+          <button
+            type="submit"
+            className="self-center py-2 text-white font-bold tracking-widest px-8 text-lg rounded-xl cursor-pointer bg-gradient-to-r to-[#027cc9cc] from-[#b300b3cc]"
+          >
+            Submit
           </button>
         </form>
-        <div className="contact-right">
-          <img src="img/right-logo.png" alt="" />
+        <div className="contact-right hidden lg:block">
+          <img src={rightLogo} alt="" className="w-80" />
         </div>
       </div>
     </section>
