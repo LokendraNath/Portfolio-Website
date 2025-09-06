38d687b62a90a5d9e700e551236220f614811f74
Lokendra Nath <nathdeveloper25@gmail.com>
2025-09-06 11:27:51 +0530

    bug fix

--- src/component/About.tsx
+++ src/component/About.tsx
@@ -39,10 +39,10 @@ const About = () => {
             </a>
           </div>
         </div>
-        <div className="hidden sm:block object-cover w-95 h-95 rounded-[50%] mix-blend-lighten">
+        <div className="hidden sm:block object-cover w-95 h-95 rounded-[50%] mix-blend-lighten bg-black shadow-[0_2.8px_2.2px_rgba(0,_0,_0,_0.034),_0_6.7px_5.3px_rgba(0,_0,_0,_0.048),_0_12.5px_10px_rgba(0,_0,_0,_0.06),_0_22.3px_17.9px_rgba(0,_0,_0,_0.072),_0_41.8px_33.4px_rgba(0,_0,_0,_0.086),_0_100px_80px_rgba(0,_0,_0,_0.12)]">
           <img
-            className="object-contain rounded-full bg-black shadow-[0_2.8px_2.2px_rgba(0,_0,_0,_0.034),_0_6.7px_5.3px_rgba(0,_0,_0,_0.048),_0_12.5px_10px_rgba(0,_0,_0,_0.06),_0_22.3px_17.9px_rgba(0,_0,_0,_0.072),_0_41.8px_33.4px_rgba(0,_0,_0,_0.086),_0_100px_80px_rgba(0,_0,_0,_0.12)]"
-            src="../../public/assets/img/lokendra.png"
+            className="object-contain rounded-full "
+            src="/assets/img/lokendra-img.png"
             alt="Lokendra Nath"
           />
         </div>
