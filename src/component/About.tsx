f1bcd72169bae613ce78da23d71a87a018b06c96
Lokendra Nath <nathdeveloper25@gmail.com>
2025-09-06 09:42:48 +0530

    about section photo change

--- src/component/About.tsx
+++ src/component/About.tsx
@@ -4,15 +4,9 @@ const About = () => {
       id="about"
       className="min-h-screen flex items-center justify-center  px-2 lg:px-15 w-full lg:py-30"
     >
-      <div className="about-container flex sm:flex-col lg:flex-row items-center justify-center gap-28">
-        <img
-          className="img hidden sm:block object-cover w-85 h-85 rotate-[-15deg] rounded-[50%]"
-          src="/src/assets/img/main-op.jpg"
-          alt=""
-        />
-
-        <div className="info-box flex flex-col text-center items-center gap-6 lg:gap-4">
-          <div className="text mb-5 lg:mb-3">
+      <div className="about-container flex sm:flex-col lg:flex-row items-center justify-center gap-22">
+        <div className="info-box flex flex-col text-center items-start gap-6 lg:gap-4">
+          <div className="text mb-5 lg:mb-3 items-start flex flex-col">
             <h3 className="text-4xl sm:text-4xl lg:text-3xl mb-3 font-semibold opacity-70 ">
               Hi, I'm
             </h3>
@@ -20,16 +14,18 @@ const About = () => {
               Lokendra Nath
             </h1>
             <span className="bg-gradient-to-r from-[#009dff] to-[#ff00ff] bg-clip-text sm:text-3xl text-transparent text-[22px] font-gothic tracking-wider">
-              Frontend Developer
+              Software Developer
             </span>
           </div>
 
-          <div className="btn-group flex gap-4 space-x-2 sm:space-x-10 lg:space-x-5">
-            <div className="btn">Resume</div>
+          <div className="btn-group flex gap-4 space-x-2 sm:space-x-10 lg:space-x-3">
+            <div className="btn">
+              Resume <i className="fa-solid fa-download ml-2"></i>
+            </div>
             <div className="btn">Contact</div>
           </div>
 
-          <div className="social space-x-10 sm:space-x-20 lg:space-x-10 mt-4 sm:mt-6 flex">
+          <div className="social space-x-10 sm:space-x-20 lg:space-x-10 mt-4 sm:mt-6 flex lg:pl-3">
             <a href="https://github.com/LokendraNath" target="_blank">
               <i className="text-3xl lg:text-4xl sm:text-5xl cursor-pointer fa-brands fa-github"></i>
             </a>
@@ -40,6 +36,13 @@ const About = () => {
               <i className="text-3xl sm:text-5xl lg:text-4xl cursor-pointer fa-brands fa-square-x-twitter"></i>
             </a>
           </div>
+        </div>
+        <div className="hidden sm:block object-cover w-95 h-95 rounded-[50%] mix-blend-lighten">
+          <img
+            className="object-contain rounded-full bg-black shadow-[0_2.8px_2.2px_rgba(0,_0,_0,_0.034),_0_6.7px_5.3px_rgba(0,_0,_0,_0.048),_0_12.5px_10px_rgba(0,_0,_0,_0.06),_0_22.3px_17.9px_rgba(0,_0,_0,_0.072),_0_41.8px_33.4px_rgba(0,_0,_0,_0.086),_0_100px_80px_rgba(0,_0,_0,_0.12)]"
+            src="/src/assets/img/Lokendra.png"
+            alt=""
+          />
         </div>
       </div>
     </section>
