c92170b7de4b1d2202116dc60ce58f7fea33f7fb
Lokendra Nath <nathdeveloper25@gmail.com>
2025-11-29 16:48:47 +0530

    hero section updated

--- component/Hero.tsx
+++ component/Hero.tsx
@@ -1,36 +1,39 @@
 "use client";
 
 import lokendraImage from "@/public/assets/img/lokendra-img.png";
-
+import { motion } from "framer-motion";
 import {
   ContactRoundIcon,
   Download,
+  DownloadCloud,
   GithubIcon,
+  LayoutDashboard,
+  LayoutGridIcon,
   LinkedinIcon,
   TwitterIcon,
 } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 const Hero = () => {
   // Icons Array
-  const icons = [
+  const skillIcons = [
+    "/assets/skills/javascript.svg",
+    "/assets/skills/typescript.svg",
+    "/assets/skills/react.svg",
+    "/assets/skills/nextjs.svg",
+    "/assets/skills/tailwindcss.svg",
+    "/assets/skills/frammer-motion.svg",
+    "/assets/skills/zustand.svg",
+    "/assets/skills/redux.svg",
     "/assets/skills/nodejs.svg",
     "/assets/skills/expressjs.svg",
-    "/assets/skills/frammer-motion.svg",
-    "/assets/skills/javascript.svg",
-    "/assets/skills/mongodb.svg",
-    "/assets/skills/nextjs.svg",
     "/assets/skills/postgresql.svg",
-    "/assets/skills/redux.svg",
-    "/assets/skills/tailwindcss.svg",
-    "/assets/skills/zustand.svg",
-    "/assets/skills/react.svg",
-    "/assets/skills/typescript.svg",
+    "/assets/skills/mongodb.svg",
   ];
-
   return (
-    <section>
-      <div className="max-w-2xl m-auto flex flex-col items-center justify-center text-center pt-24 md:pt-30 px-4 md:px-0 pb-8 overflow-hidden">
+    <section id="home" className="min-h-screen flex items-center">
+      <div className="max-w-3xl m-auto flex flex-col items-center justify-center text-center pt-24 md:pt-30 px-4 pb-8 overflow-hidden">
         {/* Profile Image */}
         <div>
           <Image
@@ -38,7 +41,7 @@ const Hero = () => {
             alt="Profile Image"
             width={150}
             height={150}
-            className="rounded-full mb-4"
+            className="rounded-full mb-4 shadow-lg"
             priority
           />
         </div>
@@ -50,6 +53,81 @@ const Hero = () => {
         <h1 className="text-3xl md:text-5xl font-semibold mt-2">
           Full Stack Developer
         </h1>
+        {/* Icon Swipe */}
+        <div className="relative my-5 md:my-7 w-full mask-r-from-50% mask-l-from-50% overflow-hidden">
+          <motion.div
+            className="flex gap-7 w-max"
+            animate={{ x: ["0%", "-50%"] }}
+            transition={{
+              repeat: Infinity,
+              repeatType: "loop",
+              duration: 15,
+              ease: "linear",
+            }}
+          >
+            {/* Icon Loop */}
+            {skillIcons.concat(skillIcons).map((icon, index) => (
+              <Image
+                key={index}
+                src={icon}
+                width={40}
+                height={40}
+                alt="Icons"
+                className="md:w-[50px] md:h-[50px]"
+              />
+            ))}
+          </motion.div>
+        </div>
+
+        {/* Buttons */}
+        <div className="flex flex-col md:flex-row items-center gap-5 w-full md:w-auto">
+          <a
+            href="#portfolio"
+            className="bg-linear-to-br from-teal-400 to-cyan-600 w-full md:w-auto py-2 px-8 text-white rounded-full font-semibold flex text-lg items-center justify-center gap-2 hover:opacity-90 transition-all duration-300 hover:shadow-sm hover:-translate-y-1"
+          >
+            Works <LayoutDashboard className="w-4" />
+          </a>
+          <a
+            href="#/assets/resume.pdf"
+            className="w-full md:w-auto py-2 px-6 rounded-full font-semibold border border-content/20 hover:border-content/40 hover:shadow-sm text-lg transition-all duration-300 flex items-center justify-center gap-2 hover:-translate-y-1"
+            download
+          >
+            Resume <DownloadCloud className="w-4" />
+          </a>
+        </div>
+        {/* Social Icons */}
+        <div className="mt-10 md:mt-5 flex gap-10">
+          <Link href="https://x.com/iamlokendranath" target="_blank">
+            <Image
+              src="/assets/social/x.png"
+              width={100}
+              height={100}
+              alt="X Logo"
+              className="w-7"
+            />
+          </Link>
+          <Link
+            href="https://www.linkedin.com/in/lokendranath/"
+            target="_blank"
+          >
+            <Image
+              src="/assets/social/linkedIn.png"
+              width={100}
+              height={100}
+              alt="LinkedIn Logo"
+              className="w-7"
+            />
+          </Link>
+          <Link href="https://github.com/LokendraNath" target="_blank">
+            <Image
+              src="/assets/social/github.png"
+              width={100}
+              height={100}
+              alt="Github Logo"
+              className="w-7"
+            />
+          </Link>
+        </div>
       </div>
     </section>
   );
