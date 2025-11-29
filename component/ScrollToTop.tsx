080edc5e112364b43a71be303864ed6bc0285039
Lokendra Nath <nathdeveloper25@gmail.com>
2025-11-29 17:39:53 +0530

    update scroll to top

--- component/ScrollToTop.tsx
+++ component/ScrollToTop.tsx
@@ -32,7 +32,7 @@ const ScrollToTopButton = () => {
       {isVisible && (
         <button
           onClick={scrollToTop}
-          className="fixed bottom-8 right-8 w-10 h-10 bg-linear-to-b to-[#027cc9cc] from-[#b300b3cc] rounded-full text-white flex items-center justify-center shadow-lg focus:outline-none focus:ring-opacity-50 z-50 cursor-pointer"
+          className="fixed bottom-8 right-8 w-10 h-10 bg-linear-to-b to-[#0891b2] from-[#2dd4bf] rounded-full text-white flex items-center justify-center shadow-lg focus:outline-none focus:ring-opacity-50 z-50 cursor-pointer"
           aria-label="Scroll to top"
         >
           <ArrowUpIcon />
