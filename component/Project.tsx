ef9851c0e892e807c2b73f8c0b7325bd17436b37
Lokendra Nath <nathdeveloper25@gmail.com>
2025-11-29 17:36:55 +0530

    project section updated

--- component/Project.tsx
+++ component/Project.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Eye, Folder } from "lucide-react";
+import { ArrowUpRight, Eye, Folder, Github } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -58,7 +58,7 @@ const Project = () => {
   return (
     <section
       id="projects"
-      className=" flex items-center justify-center flex-col min-h-screen pt-20"
+      className="flex items-center justify-center flex-col min-h-screen my-10"
     >
       <h2 className="text-center text-4xl font-bold mb-10">Projects</h2>
 
@@ -66,7 +66,7 @@ const Project = () => {
         {portfolioProjects.map((project) => (
           <div
             key={project.title}
-            className="bg-stone-100 text-center rounded-2xl mb-7 sm:mb-15 lg:mb-7 transition duration-300 shadow-lg hover:shadow-2xl hover:-translate-y-5"
+            className=" bg-stone-100 text-center rounded-2xl transform transition-all duration-300 shadow-[0px_18px_50px_-10px_rgba(0,0,0,0.2)] hover:shadow-[0px_25px_50px_-12px_rgba(0,0,0,0.25)] hover:-translate-y-2"
           >
             <Image
               className="rounded-t-2xl h-45 w-full object-cover object-top"
@@ -75,11 +75,11 @@ const Project = () => {
               src={project.image}
               alt={project.title}
             />
-            <div className="px-3 py-3 sm:py-8 lg:pt-4 flex flex-col items-center">
+            <div className="px-3 pb-3 lg:pt-4 flex flex-col items-center">
               <h3 className="text-2xl sm:text-3xl lg:text-2xl tracking-wider mb-1 font-bold bg-linear-to-r from-slate-600 to-slate-800 bg-clip-text text-transparent">
                 {project.title}
               </h3>
-              <h3 className="text-sm mb-4">{project.desc}</h3>
+              <h3 className="text-sm mb-2">{project.desc}</h3>
               <p className="text-[13px] max-w-[80%] bg-linear-to-r from-blue-600 to-violet-600 bg-clip-text text-transparent mb-3">
                 {project.techStack.join(", ")}
               </p>
@@ -89,12 +89,10 @@ const Project = () => {
                   href={project.repo}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="relative group inline-flex items-center gap-2 px-5 py-2.5 rounded-lg font-semibold overflow-hidden transition-all duration-300"
+                  className="relative group inline-flex items-center gap-2 px-5 py-1 rounded-lg font-semibold overflow-hidden transition-all duration-300"
                   style={{ background: "#181717", color: "#fff" }}
                 >
-                  {/* Hover overlay */}
-                  <span className="absolute inset-0 bg-white/10 scale-0 group-hover:scale-100 transition-transform duration-300 rounded-lg"></span>
-                  <Folder className="w-4" />
+                  <Github className="w-4" />
                   <span className="relative z-10">GitHub</span>
                 </Link>
 
@@ -103,12 +101,10 @@ const Project = () => {
                   href={project.live}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="relative group inline-flex items-center gap-2 px-5 py-2.5 rounded-lg font-semibold text-white overflow-hidden transition-all duration-300 bg-green-600 hover:bg-green-700"
+                  className="relative group inline-flex items-center gap-2 px-5 py-1 rounded-lg font-semibold text-white overflow-hidden transition-all duration-300 bg-green-600 hover:bg-green-700"
                 >
-                  {/* Gradient hover effect */}
-                  <span className="absolute inset-0 bg-linear-to-r from-green-400 to-green-700 opacity-0 group-hover:opacity-100 transition-opacity duration-300"></span>
-                  <Eye className="w-4" />
                   <span className="relative z-10">Live</span>
+                  <ArrowUpRight className="w-4" />
                 </Link>
               </div>
             </div>
